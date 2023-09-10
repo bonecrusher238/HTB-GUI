5738249ce6c4aa6826400e51dbd9069d3f761669
blank <blank@blank.com>
2023-09-10 19:51:11 -0400

    modified:   Shell-Edition/htb-go-buster.sh 	modified:   Shell-Edition/htb.sh

--- Shell-Edition/htb.sh
+++ Shell-Edition/htb.sh
@@ -1,35 +1,66 @@
 #!/bin/bash
 ver=''
 configLoc='/tmp/htb-gui/config.dat'
-setupVer= cat $configLoc | grep -w 'SETUP' | cut -d '=' -f2
+setupVer=$(cat $configLoc | grep -w 'SETUP' | cut -d '=' -f2)
+currIPADDR=$(cat $configLoc | grep -w 'currIPADDR' | cut -d '=' -f2)
 
-echo $setupVer
+setupTest() {
+    if [ ! "$setupVer" = "TRUE" ];
+        then
+            echo "You Skipped a step!"
+            echo "Running Installer!"
+            exit
+    fi
+}
 
-echo "-----------------------------------------------------------------"
-printf "\n" 
-printf "\n"
-printf "\n"
-printf "\n"
-echo "                     Hack the box GUI"
-echo "                                     Created by Bone."
-printf "\n"
-echo "  This program is simply for remembering commands and making life easier"
-echo "    This has no affiliation with anyone or any place"
-echo "      This does not claim ownership nor rights of any other reasource used"
-echo "-----------------------------------------------------------------"
-printf "\n"
-printf "\n"
-echo " Please select an option!"
-echo " 1. Select a Host IP"
-echo " 2. DNS enum"
-echo " 3. Go Buster!"
-echo " 4. burpsuite"
-echo " 5. nmap"
-read -p ":?> " Input
+mainMenu() {
+    echo "------------------------------------------------------------------------------"
+    printf "\n" 
+    printf "\n"
+    printf "\n"
+    printf "\n"
+    echo "                     Hack the box GUI"
+    echo "                                     Created by Bone."
+    printf "\n"
+    echo "  This program is simply for remembering commands and making life easier"
+    echo "    This has no affiliation with anyone or any place"
+    echo "      This does not claim ownership nor rights of any other reasource used"
+    echo "------------------------------------------------------------------------------"
+    printf "\n"
+    printf "\n"
+    echo " Please select an option!                                 Current ADDR"  
+    echo " 1. Select a Host IP                                        :?> $currIPADDR"
+    echo " 2. DNS enum                                                :?> ENUM"
+    echo " 3. Go Buster!"
+    echo " 4. burpsuite"
+    echo " 5. nmap"
+    printf "\n"
+    read -p ":?> " Input
 
 case $input in
-1 | IP)
-echo "Please enter the IPADDR"
+    1 | IP | ip)
+    echo "Please enter the IPADDR"
+    read -p ":?> " newIP
+    echo "currIPADDR=$newIP" >> $configLoc
+    ;;
+    2 | dns)
+    echo "This section is to allow your web browser to connect to a website"
+    echo $currIPADDR
+    ;;
+    3 | gb | go | gobuster)
 
-;;
+    ;;
+    4 | bp | bs | burp | burpsuite)
+    ;;
+
+    5 | nmap | np)
+    ;;
+
+    *)
+    exit
 esac
+}
+
+# Script Starts
+setupTest
+mainMenu
